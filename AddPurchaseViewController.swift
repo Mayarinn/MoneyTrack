13245a24281dd806cd97d65571a61def91977b2a
Karina Osadchaya <dyach.zero@gmail.com>
2024-09-20 10:48:43 +0300

    ADD: Title label to AddPurchase VC

--- AddPurchaseViewController.swift
+++ AddPurchaseViewController.swift
@@ -11,6 +11,15 @@ class AddPurchaseViewController: UIViewController {
 
 //MARK: - variables
     
+    var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Super Title"
+        titleLabel.font = .systemFont(ofSize: 24)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return titleLabel
+    }()
+    
 //MARK: - functions
     
     override func viewDidLoad() {
